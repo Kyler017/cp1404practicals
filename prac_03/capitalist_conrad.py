2e1a34aff0b0e92cd1e8f134178a112cb9eccd3a
Kyler <3396001991@11.com>
2025-06-07 13:08:16 +0800

    Add feature using a number_of_days variable

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -16,24 +16,3 @@ MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 day = 0
 
-price = INITIAL_PRICE
-out_file = open(FILENAME, 'w')
-print(f"starting price:${price:,.2f}",file=out_file)
-while MIN_PRICE <= price <= MAX_PRICE:
-    price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
-        price_change = random.uniform(0, MAX_INCREASE)
-    else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
-
-    price *= (1 + price_change)
-    day += 1
-    print(f"on day {day} price is:${price:,.2f}",file=out_file)
-    
-out_file.close()
