403aa54dae3ea9d978be31d4c506436b9022537c
Kyler <3396001991@11.com>
2025-06-07 12:56:15 +0800

    add code that asks the user for their name

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,18 +3,3 @@ out_file = open("name.txt","w")
 print(name,file=out_file,end="")
 out_file.close()
 
-in_file = open("name.txt","r")
-print(f"Hi!{name}!")
-in_file.close()
-
-with open("numbers.txt") as in_file:
-    i = 0
-    for line,_ in zip(in_file,range(2)):
-        i += int(line)
-    print(i)
-
-with open("numbers.txt") as in_file:
-    i = 0
-    for line in in_file:
-        i += int(line)
-    print(i)
