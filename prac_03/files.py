65df18e061f2a4a01e6d26e6041ef1c016f30dc2
Kyler <3396001991@11.com>
2025-06-07 12:58:39 +0800

    add three numbers on separate

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,13 @@ out_file = open("name.txt","w")
 print(name,file=out_file,end="")
 out_file.close()
 
+in_file = open("name.txt","r")
+print(f"Hi!{name}!")
+in_file.close()
+
+with open("numbers.txt") as in_file:
+    i = 0
+    for line,_ in zip(in_file,range(2)):
+        i += int(line)
+    print(i)
+
