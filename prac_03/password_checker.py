7155096ed6444b470f83db9bf0f0c4f62f1b7108
Kyler <3396001991@11.com>
2025-06-07 12:51:57 +0800

    add Program to get and check a user's password.

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -25,30 +25,3 @@ def main():
     print(f"Your {len(password)}-character password is valid: {password}")
 
 
-def is_valid_password(password):
-    """Determine if the provided password is valid."""
-    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
-        return False
-    number_of_lower = 0
-    number_of_upper = 0
-    number_of_digit = 0
-    number_of_special = 0
-    for character in password:
-        if character.islower():
-            number_of_lower += 1
-        if character.isupper():
-            number_of_upper += 1
-        if character.isdigit():
-            number_of_digit += 1
-        if IS_SPECIAL_CHARACTER_REQUIRED and character in SPECIAL_CHARACTERS:
-            number_of_special += 1
-    if number_of_lower == 0 or number_of_upper == 0 or number_of_digit == 0 :
-       return False
-
-    if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special ==0 :
-        return False
-
-    return True
-
-
-main()
