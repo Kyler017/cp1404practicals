c07b24cbf7fe10f52ecf329781af861db0d4b795
Kyler <3396001991@11.com>
2025-06-22 16:27:44 +0800

    Add the name of the email and get output

---
+++ prac_05/emails.py
@@ -0,0 +1,15 @@
+def main():
+    email_of_name = {}
+    email = input("please enter Email: ")
+    while email != " ":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != " ":
+            name = input("please enter Name: ")
+        email_of_name[email] = name
+        email = input("please enter Email: ")
+
+    for email, name in email_of_name.items():
+        print(f"{name} {email}")
+
+
