8491df642db850eaf2832f15b85536627270a7c5
Kyler <3396001991@11.com>
2025-07-05 22:08:23 +0800

    Add distance to the car if it has enough fuel, or drive as far as fuel allows.

--- prac_06/car.py
+++ prac_06/car.py
@@ -11,4 +11,13 @@ class Car:
     def add_fuel(self, amount):
         self.fuel += amount
 
+    def drive(self, distance):
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self._odometer += distance
+        return distance
+
 
