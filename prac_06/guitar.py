dec6a44ba57b803fb6ebacd4ec04010b2d848b3f
Kyler <3396001991@11.com>
2025-07-05 22:40:35 +0800

    Add a new guitar with name, year and cost

---
+++ prac_06/guitar.py
@@ -0,0 +1,15 @@
+"""
+Estimate: 12 mins
+Actual time: 22 mins
+"""
+
+from datetime import datetime
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
