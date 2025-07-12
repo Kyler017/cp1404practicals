f6ffa4d26179c71f677e6912bf0b19f38bb29755
Kyler <3396001991@11.com>
2025-07-12 21:31:24 +0800

    Add a new programming language with the given details

---
+++ prac_07/programming_language.py
@@ -0,0 +1,10 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic=False):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+
