c9d0e2cfcf8e523b72f291af8ccb2c63f85fa580
Kyler <3396001991@11.com>
2025-07-20 08:22:40 +0800

    add a list of names when the app starts.

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Alice", "Bob", 123, "Dora"]
+
+
