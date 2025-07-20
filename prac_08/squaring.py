ecce97c00afb6dd8e13627ffab7abe35425cd6b9
Kyler <3396001991@11.com>
2025-07-20 08:20:55 +0800

    add function to square the input number and show the result.

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -10,4 +10,13 @@ class SquareNumberApp(App):
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
+    def handle_calculate(self, value):
+        try:
+            num = float(value)
+            result = num * num
 
+            self.root.ids.output_label.text = str(result)
+        except:
+            self.root.ids.output_label.text = "Error"
+
+SquareNumberApp().run()
