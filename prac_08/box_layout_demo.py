da5bef1cddcbc70ed1a4eec641b172609091a4f5
Kyler <3396001991@11.com>
2025-07-20 08:18:58 +0800

    add greeting message using the name entered in the text box

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,4 +8,9 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        print("test")
+        name = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f"Hello {name}"
+
 
