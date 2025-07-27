ac7fa6c475f3505032014d1d52d0ef431f5841ed
Kyler <3396001991@11.com>
2025-07-27 10:33:03 +0800

    week 09 homework

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -12,3 +12,9 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        random_number = random.uniform(0, 100)
+        if random_number < self.reliability:
+            return super().drive(distance)
+        return 0
