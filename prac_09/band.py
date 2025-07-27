f35a7230289f418b0adb2234e54bee83d57c37a4
Kyler <3396001991@11.com>
2025-07-27 10:24:50 +0800

    Add a Band with name and empty list of musicians.

---
+++ prac_09/band.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical
+Band class to show aggregation: Band has Musicians.
+"""
+
+
+class Band:
+
+    def __init__(self, name=""):
+        self.name = name
+        self.musicians = []
+
