d40ef4da5e8257a6da6483c09fcb962669a2a697
Kyler <3396001991@11.com>
2025-08-03 09:06:58 +0800

    Add Repeat string s, n times, with spaces in between.

---
+++ prac_10/testing.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical
+Testing code using assert and doctest
+
+Estimate: 20 minutes
+Actual: 18 minutes
+"""
+
+import doctest
+from prac_06.car import Car
+
+
+def repeat_string(s, n):
+    repeated = [s for i in range(n)]
+    return " ".join(repeated)
+
+
+
+
+
